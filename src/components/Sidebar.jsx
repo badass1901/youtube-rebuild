91932fc406cea6e646f6e3d148ef2b43691cbfee
Shashwat Sagar <shashwatsagar19@gmail.com>
2023-02-11 16:25:01 +0530

    added api key and created fetch_from_API function

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,7 @@
 import { Stack } from "@mui/material";
 import { categories } from "../utils/constants";
 
-const selectedCategory = "New";
-const Sidebar = () => {
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => {
   return (
     <Stack
       direction="row"
@@ -11,15 +10,16 @@ const Sidebar = () => {
         height: { sx: "auto", md: "95%" },
         flexDirection: { md: "column" },
       }}
-      key={categories.name}
     >
       {categories.map((category) => (
         <button
           className="category-btn"
+          onClick={() => setSelectedCategory(category.name)}
           style={{
             background: category.name === selectedCategory && "#FC1503",
             color: "white",
           }}
+          key={category.name}
         >
           <span
             style={{
